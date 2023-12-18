dd04c036656e7597cea17e17ac3fb2b673c24b4b
Julio <juliobarrios0296@gmail.com>
2023-12-18 11:41:08 -0500

    feat: Button animation added

--- JavaScripts/animation_standingOut.js
+++ JavaScripts/animation_standingOut.js
@@ -3,15 +3,15 @@ const standingOuts = document.querySelectorAll(".animationStandingOut")
 const observerStandingOut = new IntersectionObserver(entries => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-                entry.target.classList.add("animate__animated", "animate__rubberBand", "animate__repeat-3");
+                entry.target.classList.add("animate__animated", "animate__rubberBand", "animate__repeat-2");
             } else {
-                entry.target.classList.remove("animate__animated", "animate__rubberBand", "animate__repeat-3");
+                entry.target.classList.remove("animate__animated", "animate__rubberBand", "animate__repeat-2");
             }
         });
     },
     {
         rootMargin: "100px",
-        threshold: 1,
+        threshold: 0.000000000000001,
     });
 
 standingOuts.forEach(standingOut => {
