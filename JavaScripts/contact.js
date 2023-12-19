d6e91be5e5b9c479d667e8e41dafb5deb4060dc2
Julio <juliobarrios0296@gmail.com>
2023-12-18 20:17:16 -0500

    feat: Validation and form effects added

--- JavaScripts/contact.js
+++ JavaScripts/contact.js
@@ -23,3 +23,27 @@ function sendMail(){
                 console.log(res);
             }).catch((error) => console.log(error));
 }
+
+function validationForm(){
+    const formErrorIds = ['formName', 'formCellphone', 'formEmail', 'formGrade', 'formConsultation'];
+
+    for (const id of formErrorIds) {
+        const element = document.getElementById(id);
+
+        if (!element.value.trim()) {
+            element.classList.add('animate__animated', 'animate__shakeX');
+
+            setTimeout(() => {
+                element.classList.remove('animate__animated', 'animate__shakeX');
+            }, 1000);
+        }
+    }
+
+    const isFormValid = formErrorIds.every(id => document.getElementById(id).value.trim() !== '');
+
+    if (isFormValid) {
+        sendMail();
+        $('#exampleModalToggle').modal('show');
+    }
+}
+
